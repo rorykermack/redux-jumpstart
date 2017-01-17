83abdfbfb470ef52a0c0a0c22ae9b883a8f2992d
Rory Kermack <rorykermack@gmail.com>
2017-01-17 22:49:59 +0100

    feat: rm old code

--- src/common/auth/back.firebase.config.js
+++
@@ -1,8 +0,0 @@
-/* Set up your firebase creds here */
-export default {
-  apiKey: "AIzaSyDeZiKHNrDZ8CwP3omZADr2_gIdIAPzjWc",
-  authDomain: "reactchat-1310d.firebaseapp.com",
-  databaseURL: "https://reactchat-1310d.firebaseio.com",
-  storageBucket: "reactchat-1310d.appspot.com",
-  messagingSenderId: "140278458471"
-};
