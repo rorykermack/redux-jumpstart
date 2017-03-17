b8a7a6ee2723393e2ff1eee5fd57d4aaf4cba6ca
Rory Kermack <rorykermack@gmail.com>
2017-03-17 08:56:29 +0100

    feat: addition of routing & dev-tools middleware

--- src/reducers/configureStore.js
+++ src/reducers/configureStore.js
@@ -1,12 +1,30 @@
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
 import {createStore, applyMiddleware} from 'redux';
+import {routerMiddleware} from 'react-router-redux';
+import {browserHistory} from 'react-router';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import rootReducer from './appReducer';
 
-export default function configureStore(initialState) {
+const routerMW = routerMiddleware(browserHistory);
+export default function configureStore() {
   if (module.hot) {
     /* eslint-disable */
-    console.log('=== DEV ENVIRONMENT ===');
+    console.log('=========== REDUX-JUMPSTART DEV ENVIRONMENT ===========');
     /* eslint-disable */
+    return createStore(rootReducer, composeWithDevTools(
+      applyMiddleware(thunk, routerMW)
+    ));
   }
   return createStore(rootReducer, initialState, applyMiddleware(thunk));
+
+  // @todo [Rory]: Keep state between hot reloads
+  // if (module.hot) {
+  //   console.log('=== DEV ENVIRONMENT ===');
+  //   // Enable Webpack hmr => reducers
+  //   module.hot.accept('./app/reducer', () => {
+  //     const nextReducer = require('./app/reducer');
+  //     store.replaceReducer(nextReducer);
+  //     console.log('/***** Hmr Redux Store Updated *****/');
+  //   });
+  // }
 }
